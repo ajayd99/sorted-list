79833920f5c920774ac68236f0f8fe86a2f086c9
ajayd99 <39825971+ajayd99@users.noreply.github.com>
2018-06-01 10:28:30 +0100

    Add files via upload

---
+++ ARRAY.py
@@ -0,0 +1,13 @@
+ParentsList = ["latch","louise"]
+KidsList = ["daisy","ajay"]
+PetsList = ["ted","amos","cybil"]
+
+FamilyList = ParentsList + KidsList + PetsList
+
+print("random order:" + str(FamilyList))
+
+FamilyList.sort()
+print ("alphabetical order:" + str(FamilyList))
+
+
+
